e72c74f067ad6b0c88a36f4d245adbbe5b0b5c29
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsApplication1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+namespace WindowsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeList1 = new DevExpress.XtraTreeList.TreeList();
+            this.listBoxControl1 = new DevExpress.XtraEditors.ListBoxControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.treeList1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeList1
+            // 
+            this.treeList1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeList1.Location = new System.Drawing.Point(0, 0);
+            this.treeList1.Name = "treeList1";
+            this.treeList1.Size = new System.Drawing.Size(365, 482);
+            this.treeList1.TabIndex = 0;
+            // 
+            // listBoxControl1
+            // 
+            this.listBoxControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxControl1.Location = new System.Drawing.Point(365, 0);
+            this.listBoxControl1.Name = "listBoxControl1";
+            this.listBoxControl1.Size = new System.Drawing.Size(325, 427);
+            this.listBoxControl1.TabIndex = 1;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.simpleButton1.Location = new System.Drawing.Point(365, 427);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(325, 55);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "Test";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 482);
+            this.Controls.Add(this.listBoxControl1);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.treeList1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.treeList1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraTreeList.TreeList treeList1;
+        private DevExpress.XtraEditors.ListBoxControl listBoxControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to traverse through all nodes in TreeList
+
+
+<p>This example demonstrates how the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraTreeListTreeList_NodesIteratortopic">NodesIterator</a> object can be used to traverse through all existing nodes. </p><p><i>Please note that when the TreeList works in virtual mode, child nodes are created only when an end-user expands a parent node. Until this, these nodes are not created and that is why they cannot be obtained.</i></p>
+
+<br/>
+
+
